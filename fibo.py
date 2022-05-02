a2424a6fb649750138d6ce1bd73a07dd39122a96
Christian Rodriguez <crodriguezh@ulasalle.edu.pe>
2022-05-01 21:15:35 -0500

    Agregando nombre de usuario y dirección de correo electrónico

---
+++ fibo.py
@@ -0,0 +1,15 @@
+def Iterativo_Fibonacci (Posicion, Out_Salida):
+    Ejecuta_Ahora = 0
+    Ejecuta_Despues = 1
+    for i in range (Posicion + 1):
+        if Out_Salida:
+            print(str(Ejecuta_Ahora)+ ", ", end = "")
+        Valor_Temporal = Ejecuta_Ahora
+        Ejecuta_Ahora = Ejecuta_Despues
+        Ejecuta_Despues = Ejecuta_Despues + Valor_Temporal
+    return Valor_Temporal
+
+Posicion = 10
+
+print(f"Imprimiendo Fibonacci hasta {Posicion}")
+Iterativo_Fibonacci(Posicion, True)
